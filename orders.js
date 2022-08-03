1481a0f028f6abb852201feedf26ca3881b6b258
Dipesh <dpshchhn@gmail.com>
2022-08-03 10:46:19 +0545

    js-contents

---
+++ orders.js
@@ -0,0 +1,36 @@
+const Orders =[{
+    productName:'Foldable Mini Drone',
+    ProdctNumber:'85631',
+    paymentStatus:'Due',
+    shipping:'Declined'
+},
+{
+    productName:'KF102 Derone',
+    ProdctNumber:'36378',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName:'Ruko F11 pro Drone',
+    ProdctNumber:'49347',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Dron with Camera Drone',
+    ProdctNumber:'96996',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'GPS 4k Drone',
+    ProdctNumber:'22821',
+    paymentStatus:'Paid',
+    shipping:'Deliverd'
+},
+{
+    productName:'Razor Drone',
+    ProdctNumber:'98246',
+    paymentStatus:'Due',
+    shipping:'Ready'
+}]
